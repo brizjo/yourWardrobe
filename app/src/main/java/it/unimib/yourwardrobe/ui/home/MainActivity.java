566f8377e745931b2a96001e2724810c8c4b1c4a
AndreaT-T <a.turra4@campus.unimib.it>
2025-12-11 17:09:13 +0100

    ciao

--- app/src/main/java/it/unimib/yourwardrobe/ui/home/MainActivity.java
+++ app/src/main/java/it/unimib/yourwardrobe/ui/home/MainActivity.java
@@ -1,4 +1,4 @@
-package it.unimib.yourwardrobe;
+package it.unimib.yourwardrobe.ui.home;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import it.unimib.yourwardrobe.R;
 
 public class MainActivity extends AppCompatActivity {
 

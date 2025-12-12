ac97981ad8882dd39666d1fda66cfea061ca7dd8
brizjo <fabrizio.fusarrif@gmail.com>
2025-12-12 21:04:08 +0100

    Aggiunto immagine e messo la parte testuale in una scrollview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,14 +4,12 @@ plugins {
 
 android {
     namespace = "it.unimib.yourwardrobe"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "it.unimib.yourwardrobe"
         minSdk = 29
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -28,8 +26,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
@@ -43,4 +41,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation(libs.lottie)
+
 }

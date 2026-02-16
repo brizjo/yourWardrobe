4dbb59c0771ae413af99707a08fe2872c6ebe591
samuele.villa <samuele.villa.ufficiale@gmail.com>
2026-02-16 13:40:05 +0100

    feat: added quick actions to home

--- app/src/main/java/it/unimib/yourwardrobe/ui/main/MainActivity.java
+++ app/src/main/java/it/unimib/yourwardrobe/ui/main/MainActivity.java
@@ -44,23 +44,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.top_bar), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, 0, systemBars.right, 0);
-            MaterialToolbar toolbar = findViewById(R.id.top_bar);
-            if (toolbar != null) {
-                toolbar.setPadding(
-                        toolbar.getPaddingLeft(),
-                        systemBars.top,
-                        toolbar.getPaddingRight(),
-                        toolbar.getPaddingBottom()
-                );
-            }
-            BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
-            if (bottomNav != null) {
-                bottomNav.setPadding(0, 0, 0, systemBars.bottom);
-            }
-
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0);
+            return insets;
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottom_nav), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom);
             return insets;
         });
 
@@ -117,6 +108,7 @@ public class MainActivity extends AppCompatActivity {
                         .build();
                 navController.navigate(item.getItemId(), null, navOptions);
             });
+
             MaterialToolbar toolbar = findViewById(R.id.top_bar);
             setSupportActionBar(toolbar);
             if (getSupportActionBar() != null) {
@@ -124,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
             }
             Set<Integer> topLevelDestinations = new HashSet<>();
             topLevelDestinations.add(R.id.homeFragment);
-            topLevelDestinations.add(R.id.wardrobe_nav_graph);
+            topLevelDestinations.add(R.id.wardrobeFragment);
             topLevelDestinations.add(R.id.personalStylistFragment);
             AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(topLevelDestinations).build();
             NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
